2ceecb4036096fd12114e0dc406e973c635480f2
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-10-11 16:23:56 +0200

    Removed some unneeded stuff from the bundle class.

--- KrisFacebookBundle.php
+++ KrisFacebookBundle.php
@@ -3,14 +3,7 @@
 namespace Bundle\Kris\FacebookBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Bundle\Kris\FacebookBundle\DependencyInjection\FacebookExtension;
 
 class KrisFacebookBundle extends Bundle
 {
-    public function buildContainer(ParameterBagInterface $parameterBag)
-    {
-        ContainerBuilder::registerExtension(new FacebookExtension());
-    }
 }
