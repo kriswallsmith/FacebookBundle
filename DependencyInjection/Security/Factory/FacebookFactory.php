011d49a4a171c7adb15e4a07df83845f932adcb7
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-01-21 22:20:16 +0100

    updated FacebookFactory for the recent changes

--- DependencyInjection/Security/Factory/FacebookFactory.php
+++ DependencyInjection/Security/Factory/FacebookFactory.php
@@ -8,22 +8,36 @@ use Symfony\Bundle\FrameworkBundle\DependencyInjection\Security\Factory\Security
 
 class FacebookFactory implements SecurityFactoryInterface
 {
-    public function create(ContainerBuilder $container, $id, $config, $userProvider, $defaultEntryPoint)
+    public function create(ContainerBuilder $container, $id, $config, $userProviderId, $defaultEntryPoint)
     {
-        $provider = 'security.authentication.provider.pre_authenticated.'.$id;
-        $container
-            ->register($provider, '%security.authentication.provider.pre_authenticated.class%')
-            ->setArguments(array(new Reference($userProvider), new Reference('security.account_checker')))
-        ;
+        if ($userProviderId != 'fos_facebook.auth') {
+            $providerId = 'fos_facebook.auth.'.$id;
+            $provider = clone $container->getDefinition('fos_facebook.auth');
+
+            $arguments = $provider->getArguments();
+            $arguments[] = new Reference($userProviderId);
+            $arguments[] = new Reference('security.account_checker');
+
+            $provider->setArguments($arguments);
+
+            $container->setDefinition($providerId, $provider);
+        }
 
         // listener
         $listenerId = 'fos_facebook.security.authentication.listener.'.$id;
         $listener = $container->setDefinition($listenerId, clone $container->getDefinition('fos_facebook.security.authentication.listener'));
         $arguments = $listener->getArguments();
-        $arguments[1] = new Reference($provider);
+        $arguments[1] = new Reference($providerId);
+
+        if (is_array($config)) {
+            $options = $container->getParameter('fos_facebook.security.authentication.options');
+            $options = array_merge($options, $config);
+            $arguments[2] = $options;
+        }
+
         $listener->setArguments($arguments);
 
-        return array($provider, $listenerId, 'fos_facebook.security.authentication.entry_point');
+        return array($providerId, $listenerId, 'fos_facebook.security.authentication.entry_point');
     }
 
     public function getPosition()
