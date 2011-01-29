5c33c031f4e91b3f4ff0f5f7ef52ae6ce4dfdd5a
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-01-29 22:31:06 +0100

    first step towards using AbstractFactory() (still getting 'Circular reference detected for service "security.context" (services currently loading: security.firewall.map.context.form, security.context, security.authentication.manager, profiler_listener, profiler).'

--- DependencyInjection/Security/Factory/FacebookFactory.php
+++ DependencyInjection/Security/Factory/FacebookFactory.php
@@ -4,61 +4,10 @@ namespace FOS\FacebookBundle\DependencyInjection\Security\Factory;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\SecurityFactoryInterface;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\AbstractFactory;
 
-class FacebookFactory implements SecurityFactoryInterface
+class FacebookFactory extends AbstractFactory
 {
-    public function create(ContainerBuilder $container, $id, $config, $userProviderId, $defaultEntryPoint)
-    {
-        $providerId = 'fos_facebook.auth';
-        if ($userProviderId !== $providerId) {
-            $provider = clone $container->getDefinition($providerId);
-
-            $arguments = $provider->getArguments();
-            $arguments[] = new Reference($userProviderId);
-            $arguments[] = new Reference('security.account_checker');
-
-            $provider->setArguments($arguments);
-
-            $providerId.= '.'.$id;
-            $container->setDefinition($providerId, $provider);
-        }
-
-        $listenerId = 'fos_facebook.security.authentication.listener';
-        $listener = clone $container->getDefinition($listenerId);
-
-        $arguments = $listener->getArguments();
-        $arguments[1] = new Reference($providerId);
-
-        $options = array(
-            'check_path'                     => '/login_check',
-            'login_path'                     => '/login',
-            'use_forward'                    => false,
-            'always_use_default_target_path' => false,
-            'default_target_path'            => '/',
-            'target_path_parameter'          => '_target_path',
-            'use_referer'                    => false,
-            'failure_path'                   => null,
-            'failure_forward'                => false,
-        );
-        foreach (array_keys($options) as $key) {
-            if (isset($config[$key])) {
-                $options[$key] = $config[$key];
-            }
-        }
-        $arguments[2] = $options;
-
-        $container->setParameter('fos_facebook.security.authentication.options', $options);
-        $container->setParameter('fos_facebook.security.authentication.check_path', $options['check_path']);
-
-        $listener->setArguments($arguments);
-
-        $listenerId.= '.'.$id;
-        $container->setDefinition($listenerId, $listener);
-
-        return array($providerId, $listenerId, 'fos_facebook.security.authentication.entry_point');
-    }
-
     public function getPosition()
     {
         return 'pre_auth';
@@ -68,4 +17,38 @@ class FacebookFactory implements SecurityFactoryInterface
     {
         return 'fos_facebook';
     }
+
+    protected function getListenerId()
+    {
+        return 'fos_facebook.security.authentication.listener';
+    }
+
+    protected function createAuthProvider(ContainerBuilder $container, $id, $options, $userProviderId)
+    {
+        $authProviderId = 'fos_facebook.auth';
+        if ($userProviderId !== $authProviderId) {
+            $provider = $authProviderId.$id;
+            $container
+                ->setDefinition($provider, new DefinitionDecorator($authProviderId))
+                ->addArgument(new Reference($userProviderId))
+                ->addArgument(new Reference('security.account_checker'))
+                ->addArgument($id)
+            ;
+        }
+
+        return $provider;
+    }
+
+    protected function createEntryPoint($container, $id, $config, $defaultEntryPointId)
+    {
+        $options = $this->getOptionsFromConfig($config);
+
+        $container->getDefinition($defaultEntryPointId);
+        $arguments = $container->getArguments();
+        $arguments[1]['next'] = $options['check_path'];
+        $container->setArguments($arguments);
+
+        return $defaultEntryPointId;
+    }
+
 }
