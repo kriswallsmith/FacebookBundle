8fd8d651d055157799b1cd9a6ee90fe00c8dd36e
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-02-26 18:42:09 +0100

    fixed test

---
+++ Tests/DependencyInjection/FOSFacebookExtensionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace FOS\FacebookBundle\Tests\DependencyInjection;
+
+use FOS\FacebookBundle\DependencyInjection\FOSFacebookExtension;
+
+class FOSFacebookExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers FOS\FacebookBundle\DependencyInjection\FOSFacebookExtension::load
+     */
+    public function testLoadFailure()
+    {
+//        $this->setExpectedException('\Symfony\Component\Config\Definition\Exception\InvalidConfigurationException');
+
+        $container = $this->getMockBuilder('Symfony\\Component\\DependencyInjection\\ContainerBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $extension = $this->getMockBuilder('FOS\\FacebookBundle\\DependencyInjection\\FOSFacebookExtension')
+            ->getMock();
+
+        $extension->load(array(array()), $container);
+    }
+
+    /**
+     * @covers FOS\FacebookBundle\DependencyInjection\FOSFacebookExtension::load
+     */
+    public function testLoadSetParameters()
+    {
+        $container = $this->getMockBuilder('Symfony\\Component\\DependencyInjection\\ContainerBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $parameterBag = $this->getMockBuilder('Symfony\Component\DependencyInjection\ParameterBag\\ParameterBag')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $parameterBag
+            ->expects($this->any())
+            ->method('add');
+
+        $container
+            ->expects($this->any())
+            ->method('getParameterBag')
+            ->will($this->returnValue($parameterBag));
+
+        $extension = new FOSFacebookExtension();
+        $configs = array(
+            array('class' => array('api' => 'foo')),
+            array('file' => 'foo'),
+            array('app_id' => 'foo'),
+            array('secret' => 'foo'),
+            array('cookie' => 'foo'),
+            array('domain' => 'foo'),
+            array('logging' => 'foo'),
+            array('culture' => 'foo'),
+            array('permissions' => array('email')),
+        );
+        $extension->load($configs, $container);
+    }
+}
