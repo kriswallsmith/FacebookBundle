de0914d100fdb276a9c58e5b41916faed826b380
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-06-08 16:14:35 +0200

    s/user/facebook

--- Tests/Command/TestUsersListCommandTest.php
+++ Tests/Command/TestUsersListCommandTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the FOSUserBundle package.
+ * This file is part of the FOSFacebookBundle package.
  *
  * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
  *
