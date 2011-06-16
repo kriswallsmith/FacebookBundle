8c6b8f82bf1bb4e445ee002a24d194b986c79838
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-06-16 20:08:42 +0200

    removed refreshUser() from auth provider

--- Security/Authentication/Provider/FacebookProvider.php
+++ Security/Authentication/Provider/FacebookProvider.php
@@ -82,14 +82,4 @@ class FacebookProvider implements AuthenticationProviderInterface
 
         return new FacebookUserToken($user, $user->getRoles());
     }
-
-    /**
-     * Refresh a user
-     *
-     * @param UserInterface $user
-     */
-    public function refreshUser(UserInterface $user)
-    {
-        throw new UnsupportedUserException('User is not supported.');
-    }
 }
