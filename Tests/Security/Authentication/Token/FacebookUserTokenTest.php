0bcee3316047fa36cf312becc9681239b6c4c50b
Leszek <leszek.prabucki@gmail.com>
2011-09-05 23:13:16 +0200

    Added tests for FOS\FacebookBundle\Security\Authentication\Token\FacebookUserToken

---
+++ Tests/Security/Authentication/Token/FacebookUserTokenTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the FOSFacebookBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\FacebookBundle\Tests\Security\Authentication\Token;
+
+use FOS\FacebookBundle\Security\Authentication\Token\FacebookUserToken;
+
+class FacebookUserTokenTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provider
+     */
+    public function testThatAlwaysReturnEmptyCredentials($uid, $roles)
+    {
+        $token = new FacebookUserToken($uid, $roles);
+
+        $this->assertEquals('', $token->getCredentials());
+    }
+
+    /**
+     * @return array
+     */
+    public static function provider()
+    {
+        return array(
+            array('', array()),
+            array('l3l0', array()),
+            array('', array('role1', 'role2')),
+            array('l3l0', array('role1', 'role2'))
+        );
+    }
+}
