99e12388ff9c60ebfa489e8bf0bc89b8370c820f
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-11-12 12:36:18 +0100

    travis-ci

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,22 +9,8 @@
  * file that was distributed with this source code.
  */
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->registerPrefixes(array(
-    'Twig_' => $_SERVER['TWIG'],
-));
-$loader->register();
-
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'FOS\\FacebookBundle\\')) {
-        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        require_once __DIR__.'/../'.$path;
-        return true;
-    }
-});
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
+}
