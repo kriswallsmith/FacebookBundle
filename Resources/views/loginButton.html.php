9c1ed03209d4e250fa77495dd3c2819297e1beb3
Diego Oliveira <contato@diegoholiveira.com>
2013-06-08 12:37:27 -0300

    Fixing the php template for the login button

--- Resources/views/loginButton.html.php
+++ Resources/views/loginButton.html.php
@@ -1,3 +1,3 @@
-<div data-show-faces="<?php echo $showFaces; ?>" data-width="<?php echo $width; ?>" data-max-rows="<?php echo $maxRows; ?>" data-scope="<?php echo $scope; ?>" data-registration-url="<?php echo $registrationUrl; ?>" data-size="<?php echo $size; ?>"  data-autologoutlink="<?php echo $autologoutlink; ?>" data-onlogin="<?php echo $onlogin; ?>">
+<div class="fb-login-button" data-show-faces="<?php echo $showFaces; ?>" data-width="<?php echo $width; ?>" data-max-rows="<?php echo $maxRows; ?>" data-scope="<?php echo $scope; ?>" data-registration-url="<?php echo $registrationUrl; ?>" data-size="<?php echo $size; ?>"  data-autologoutlink="<?php echo $autologoutlink; ?>" data-onlogin="<?php echo $onlogin; ?>">
   <?php echo $label; ?>
 </div>
